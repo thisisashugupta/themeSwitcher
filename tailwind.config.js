6a2d13f80bf1cc755cf11f96a38def9e232290bb
thisisashugupta <ashugpt1812@gmail.com>
2023-10-14 01:39:43 +0530

    added all files

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,tsx, jsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
